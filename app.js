81ef68565a71335dd06bff6ffdeab98d6d438914
jakobsan1 <jakobsanjuan@gmail.com>
2024-09-14 23:31:34 +0100

    New website commit

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 // Necessary piece of code to allow a set of commands when it initially starts running
-document.addEventListener("DOMContentLoaded", () => {
+// *document.addEventListener("DOMContentLoaded", () => {
     // Grabs all the images from the html code
-    const images = document.querySelectorAll("img")
+    // *const images = document.querySelectorAll("img")
 
     // It then looks through all the images and it finds a random dog image using the fetch("url").
-    for(const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random")
-        .then(response=> response.json())
+    // *for(const image of images){
+        // fetch("https://dog.ceo/api/breeds/image/random")
+        // .then(response=> response.json())
         // The returned message is then the image of the dogs
-        .then(data => {
-            image.src = data.message
+        // .then(data => {
+            // image.src = data.message
             // Width properties
-            image.width = 100;
+            // image.width = 100;
             // Height properties
-            image.height = 100;
+            // image.height = 100;
 
 
-        })
-    }
+        // })
+    // }
 
-})
+// })
